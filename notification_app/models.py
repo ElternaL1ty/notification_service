0520f89e3e9afdc5f0696f31cea61c88697da109
ElternaL1ty <elternal1ty47@gmail.com>
2022-04-07 13:18:55 +0300

    Initial commit

---
+++ notification_app/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.core.validators import RegexValidator
+
+
+class Client(models.Model):
+    id = models.IntegerField(primary_key=True, blank=False)
+    PhoneValidator = RegexValidator(regex='/^7[0-9]{10}/', message='Телефон должен быть введен в формате 7XXXXXXXXXX')
+    phone = models.CharField(max_length=11, blank=False, validators=[PhoneValidator])
+    operator_code = models.IntegerField(blank=False)
+    tag = models.CharField(max_length=20, blank=False)
+    UtcValidator = RegexValidator(regex='/UTC[+?-][0-9]{1,2}/', message='Часовой пояс вводится в формате UTC-X или UTC-XX')
+    utc = models.CharField(max_length=6, blank=False, validators=[UtcValidator])
+
+
+class Notification(models.Model):
+    id = models.IntegerField(primary_key=True, blank=False)
+    start_datetime = models.DateTimeField(blank=False)
+    end_datetime = models.DateTimeField(blank=False)
+    message_text = models.TextField(blank=False)
+    operator_code_filter = models.JSONField(blank=True, null=True)
+    tag_filter = models.JSONField(blank=True, null=True)
+
+
+class Message(models.Model):
+    id = models.IntegerField(primary_key=True, blank=False)
+    sending_datetime = models.DateTimeField(blank=False, auto_now=True)
+    sending_status = models.CharField(max_length=10)
+    notification_id = models.ForeignKey(Notification, on_delete=models.RESTRICT)
+    client = models.ForeignKey(Client, on_delete=models.RESTRICT)
