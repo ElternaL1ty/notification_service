19d8d2378d850543a5d2df17079f1f5dd54546b0
ElternaL1ty <elternal1ty47@gmail.com>
2022-04-08 20:06:36 +0300

    added requeue for unsuccessful messages

--- notification_app/views.py
+++ notification_app/views.py
@@ -1,3 +1,5 @@
+from django.core import serializers
+from django.forms import model_to_dict
 from django.shortcuts import render
 from drf_spectacular.utils import OpenApiResponse, extend_schema, extend_schema_view
 from rest_framework import status
@@ -10,7 +12,7 @@ from .models import Client, Notification, Message
 
 from notification_service.celery import app
 
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 import requests
 import json
 
@@ -79,6 +81,34 @@ def _getlist_helper(data):
 
 
 @app.task
+def send_message(data, client, obj):
+    msg = Message.objects.filter(id=obj['id'])[:1].get()
+    r = requests.post('https://probe.fbrq.cloud/v1/send/' + str(obj['id']), data=json.dumps({
+        "id": int(obj['id']),
+        "phone": int(Client.objects.filter(id=client)[:1].get().phone),
+        "text": Notification.objects.filter(id=data['id'])[:1].get().message_text,
+    }),
+      headers={
+          "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODA2MDg1ODYsImlzcyI6ImZhYnJpcXVlIiwibmFtZSI6IlN3YWVhbWkifQ.qg14tI8ZTKp41NqNETST9cautCCp6WsKgsizX6v-FAc"
+      })
+    if r.status_code == 200:
+        print("Message delievered successfully (id: " + str(obj['id']) + ")")
+        msg.sending_status = "SUCCESS"
+        msg.save()
+    else:
+        end_dt = datetime.fromisoformat(data['end_datetime'][:-1]).astimezone(timezone.utc)
+        if end_dt>=datetime.now(tz=timezone.utc)+timedelta(seconds=600):
+            send_message.apply_async(args=[data, client, obj], countdown=600)
+            msg.sending_status = "IN QUEUE"
+            msg.save()
+            print("Message not delievered (id: " + str(obj['id']) + "). Next try in 10 minutes")
+        else:
+            msg.sending_status = "ERROR"
+            msg.save()
+            print("Message not delievered (id: " + str(obj['id']) + "). Notification is over")
+
+
+@app.task
 def start_notification(data):
 
     # --------------------- GETTING LIST OG CLIENT IDS FOR NOTIFICATIONS -------------------------------
@@ -102,24 +132,11 @@ def start_notification(data):
     for client in clients:
         obj = Message.objects.create(sending_datetime=datetime.now(), sending_status="IN QUEUE", notification_id=Notification.objects.filter(id=data['id'])[:1].get(), client=Client.objects.filter(id=client)[:1].get())
         obj.save()
+        send_message.apply_async(args=[data, client, model_to_dict(obj)])
 
-        r = requests.post('https://probe.fbrq.cloud/v1/send/'+str(obj.pk), data=json.dumps({
-            "id": int(obj.pk),
-            "phone": int(Client.objects.filter(id=client)[:1].get().phone),
-            "text": Notification.objects.filter(id=data['id'])[:1].get().message_text,
-        }),
-             headers={
-            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODA2MDg1ODYsImlzcyI6ImZhYnJpcXVlIiwibmFtZSI6IlN3YWVhbWkifQ.qg14tI8ZTKp41NqNETST9cautCCp6WsKgsizX6v-FAc"
-        })
 
-        if r.status_code == 200:
-            print("Message delievered successfully (id: " + str(obj.id)+")")
-            obj.sending_status = "SUCCESS"
-            obj.save()
-        else:
-            print("Message not delievered (id: " + str(obj.id)+")")
-            obj.sending_status = "ERROR"
-            obj.save()
+
+
 
 
 @extend_schema_view(
@@ -180,8 +197,8 @@ class NotificationViewSet(ModelViewSet):
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
-        start_dt = datetime.strptime(request.data['start_datetime'], '%Y-%m-%d %H:%M:%S').astimezone(timezone.utc)
-        end_dt = datetime.strptime(request.data['end_datetime'], '%Y-%m-%d %H:%M:%S').astimezone(timezone.utc)
+        start_dt = datetime.fromisoformat(request.data['start_datetime']).astimezone(timezone.utc)
+        end_dt = datetime.fromisoformat(request.data['end_datetime']).astimezone(timezone.utc)
         start_notification.apply_async(args=(json.dumps(serializer.data),), eta=start_dt, expires=end_dt)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
